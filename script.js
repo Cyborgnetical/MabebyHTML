44ba9ad0143fc5a636d6e9414a27fa9d7fb219f3
Cyborgnetical <140975796+Cyborgnetical@users.noreply.github.com>
2024-05-12 19:57:03 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+window.addEventListener("load",()=>{
+    let mydiv =  document.createElement("div")
+mydiv.id = "Cyborgnetical-div-There-is-no-way-something-would-be-named-this-Just-in-case-here-is-a-hash-9ea7e866262a10d0507036f097d3a9bf2bbcac109b3c9c7125016e677858e4d9"
+mydiv.innerHTML = `
+<span>Made by</span> <a href="https://github.com/Cyborgnetical?tab=overview&from=2024-05-01&to=2024-05-12">Cyborgnetical</a>
+<img src="https://avatars.githubusercontent.com/u/140975796?v=4">
+<style>
+    #Cyborgnetical-div-There-is-no-way-something-would-be-named-this-Just-in-case-here-is-a-hash-9ea7e866262a10d0507036f097d3a9bf2bbcac109b3c9c7125016e677858e4d9{
+    position: fixed;
+    display: flex;
+    align-items: center;
+    right: 0px;
+    bottom: 0px;
+    background-color: rgba(120, 255, 105, 0.5);
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    border-top-left-radius: 5px;
+    }
+    #Cyborgnetical-div-There-is-no-way-something-would-be-named-this-Just-in-case-here-is-a-hash-9ea7e866262a10d0507036f097d3a9bf2bbcac109b3c9c7125016e677858e4d9 img{
+    width: 25px;
+    height: 25px;
+    }
+    #Cyborgnetical-div-There-is-no-way-something-would-be-named-this-Just-in-case-here-is-a-hash-9ea7e866262a10d0507036f097d3a9bf2bbcac109b3c9c7125016e677858e4d9 span{
+    padding: 5px;
+    }
+</style>
+`
+document.body.appendChild(mydiv)
+})
